e75fd1524e549d7ab46adc62c3357b19ac2f2319
Andres Mazo <andrez.mazo@gmail.com>
2021-12-07 19:43:37 -0500

    agregado dos lineas

---
+++ index.js
@@ -0,0 +1,10 @@
+// git --version
+// mdir proyecto crea la carpeta 
+// Para inicializar el git se pone "git init"
+// para saber el status "git status"
+// para hacer seguimientos  "git add ."
+// muestra los cambios que se han hecho "git.log"
+// git commit - m "crear un archivo y crear un console log"
+// borra rm -r
+
+console.log ("hola mundo")
