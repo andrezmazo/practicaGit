dab6363b2284c5269ed6ccb490ed67a7feba8421
Andres Mazo <andrez.mazo@gmail.com>
2021-12-07 20:24:30 -0500

    cambios

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 // mdir proyecto crea la carpeta 
 // Para inicializar el git se pone "git init"
 // para saber el status "git status"
-// para hacer seguimientos  "git add ."
-// muestra los cambios que se han hecho "git.log"
+// para hacer seguimientos  "git add ." o el nombre del archivo en ve del punto 
 // git commit - m "crear un archivo y crear un console log"
+// muestra los cambios que se han hecho "git.log"
+// git config user.name "andres "
+// git config user.email "andre.mazo@gmail.com "
+// para hacer cambios se tiene que dar git add . antes de hacer el commit
+// git config --global user.mail "" 
+// git remote origin -muestra ayudas
+// git remote add - añadir el repositorio  se le llama origin por costumbre 
+// git remote -v muestra el repositorio 
+// git remote --help ayuda 
+// borrar el remote origin  git remote remove origin
+// git push origin master // sube el archivo 
+// developers setigs en github para generar tokens 
+//git clone pagina / se clona todos los archivos de la pagina 
 // borra rm -r
 
+
 console.log ("hola mundo")
+console.log ("linea 2")
+console.log ("linea 3")
+console.log ("linea 4")
+console.log ("linea 5")
